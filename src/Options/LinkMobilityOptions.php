a1fcc61ffdffea356e524c98d7caa73488e71c09
Tom Roar Furunes <tom.roar@halloverden.no>
2023-06-08 18:49:25 +0200

    ADD: getMessageType + setMessageType in options

--- src/Options/LinkMobilityOptions.php
+++ src/Options/LinkMobilityOptions.php
@@ -6,6 +6,7 @@ use Symfony\Component\Notifier\Message\MessageOptionsInterface;
 
 final class LinkMobilityOptions implements MessageOptionsInterface {
   const OPTION_SESSION_DATA = 'session_data';
+  const OPTION_MESSAGE_TYPE = 'message_type';
 
   public function __construct(private array $options = []) {
   }
@@ -16,6 +17,17 @@ final class LinkMobilityOptions implements MessageOptionsInterface {
 
   public function setSessionData(string $sessionData): self {
     $this->options[self::OPTION_SESSION_DATA] = $sessionData;
+    return $this;
+  }
+
+  public function getMessageType(): LinkMobilityMessageType {
+    return isset($this->options[self::OPTION_MESSAGE_TYPE])
+      ? LinkMobilityMessageType::from($this->options[self::OPTION_MESSAGE_TYPE])
+      : LinkMobilityMessageType::PLAIN_TEXT;
+  }
+
+  public function setMessageType(LinkMobilityMessageType $messageType): self {
+    $this->options[self::OPTION_MESSAGE_TYPE] = $messageType->value;
     return $this;
   }
 
