6af2d6a0cf7a8c85aab1cfcac94677b77f5de67b
Tom Roar Furunes <tom.roar@halloverden.no>
2023-06-09 13:51:50 +0200

    RM: comment

--- src/Options/LinkMobilityMessageType.php
+++ src/Options/LinkMobilityMessageType.php
@@ -4,5 +4,5 @@ namespace HalloVerden\LinkMobilityNotifierBundle\Options;
 
 enum LinkMobilityMessageType : string {
   case PLAIN_TEXT = '1'; // Non GSM 7-Bit chars will be converted to '?'
-  CASE UNICODE = '9'; // Message text should be the HEX representation of a UCS-2 encoded message
+  CASE UNICODE = '9';
 }
